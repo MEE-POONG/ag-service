6b74f70089ad367bd6c80e1ddd4d1d506d9a9461
devilzeros <devilzeros@outlook.co.th>
2021-07-03 05:13:17 +0700

    est

--- controllers/ag.controller.js
+++ controllers/ag.controller.js
@@ -1,7 +1,10 @@
 const delay = require("delay");
 const puppeteer = require('puppeteer'); +
   require('dotenv').config()
-const { masterTopPass, agenTopPass, masterSixPass, agenSixPass, adminuser, adminpass } = process.env
+const { createWorker } = require("tesseract.js");
+const worker = createWorker();
+const chalk = require('chalk');
+const { agenTopPass, agenSixPass } = process.env
 const arrayAG = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 const args = [
   '--start-maximized',
@@ -47,58 +50,93 @@ const { sanitizeBody } = require('express-validator')
 var mongoose = require('mongoose')
 
 var apiResponse = require('../helpers/apiResponse')
+async function tesseractGet(imagePath) {
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  await worker.setParameters({
+    tessedit_char_whitelist: "0123456789",
+  });
+  const {
+    data: { text },
+  } = await worker.recognize(imagePath);
+  await worker.terminate();
+  return text;
+}
 
 exports.agStoreUser = [
 
   async (req, res) => {
     try {
       const { usernameAG, webname, countUser, customerLatest } = req.body
-      const browser = await puppeteer.launch({ headless: false, defaultViewport: { width: 1920, height: 1080 }, args });
+      const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 1920, height: 1080 }, args });
       const page = await browser.newPage();
+      const captchaPath = 'captcha' + '.png';
       let element, formElement, tabs;
 
-      await page.goto(`http://ocean.isme99.com/Public/Default11.aspx?lang=EN-US`, { waitUntil: 'networkidle2' });
+      await page.goto(`http://ag.ufa6666.com/Public/Default11.aspx?lang=EN-US`, { waitUntil: 'networkidle2' });
+      await page.waitForSelector("#divImgCode > img"); // Method to ensure that the element is loaded
+      const captcha = await page.$("#divImgCode > img"); // captcha is the element you want to capture
+      await captcha.screenshot({
+        path: captchaPath,
+      });
+
       element = await page.$x(`//*[@id="txtUserName"]`);
       await element[0].type(usernameAG);
       element = await page.$x(`//*[@id="txtPassword"]`);
       await element[0].type(webname === "UFA-66" ? agenSixPass : webname === "TOP-168" ? agenTopPass : "");
-      element = await page.$x(`//*[@id="btnSignIn"]`);
-      await element[0].click();
+      await tesseractGet(captchaPath)
+        .then(async (result) => {
+          console.log(result);
+          element = await page.$x(`//*[@id="txtCode"]`);
+          await element[0].type(result);
+        })
+        .catch(function (err) {
+          console.log(chalk.red(err));
+        });
+
       await delay(5000);
+      const title = await page.title();
+      const urls = await page.url();
+
+      console.log("Page Title : " + title);
+      console.log("Page URL : " + urls);
+
       for (const [idx, data] of arrayAG.entries()) {
 
 
-        console.log("ag in for : ",idx);
-
+  
+        console.log(chalk.black.bold.bgYellow("ag in for : ", idx))
         setUserNumber = (+customerLatest.substring(countUser) + idx)
           .toString()
           .padStart(4, '0')
-          console.log(setUserNumber," : ",customerLatest);
-        await page.goto(`https://ocean.isme99.com/_SubAg/MemberList.aspx`, { waitUntil: 'networkidle2' });
+          console.log(chalk.black.bold.bgYellow(setUserNumber, " : ", customerLatest));
+        await page.goto(`https://ag.ufa6666.com/_SubAg/MemberList.aspx`, { waitUntil: 'networkidle2' });
         element = await page.$x(`//*[@id="MemberList_cm1_g_ctl02_btnCopy"]`);
-        console.log("79 : ",idx);
+        console.log(chalk.black.bold.bgYellow("79 : ", idx));
         await element[0].click();
-        console.log("81 : ",idx);
+        console.log(chalk.black.bold.bgYellow("81 : ", idx));
         await delay(1000);
         element = await page.$x(`//*[@id="txtUserName"]`);
         await element[0].type(setUserNumber);
-        console.log("85 : ",idx);
-        console.log("ag check username : ",idx," : ",setUserNumber);
+        console.log(chalk.black.bold.bgYellow("85 : ", idx));
+        console.log(chalk.black.bold.bgYellow("ag check username : ", idx, " : ", setUserNumber));
         element = await page.$x(`//*[@id="txtPassword"]`);
         await element[0].type(`Aa123456+`);
         element = await page.$x(`//*[@id="txtTotalLimit"]`);
         await element[0].type(`0`);
         element = await page.$x(`//*[@id="btnSave"]`);
         await element[0].click();
-        console.log("ag seve customer for : ",idx);
+        console.log(chalk.white.bgGreen.bold("ag seve customer for : ", idx));
         await delay(1000);
 
       }
-      await browser.close();
-       return apiResponse.successResponseWithData(res, 'Operation success', {})
-       
+      await page.close(); // Close the website
+      return apiResponse.successResponseWithData(res, 'Operation success', {})
+
     } catch (error) {
       return apiResponse.ErrorResponse(res, error)
     }
+
   }
 ]
