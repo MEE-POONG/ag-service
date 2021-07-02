ac39c92ecc55e3ae2d608313f962727fad64fed5
devilzeros <devilzeros@outlook.co.th>
2021-07-03 05:15:55 +0700

    ess

--- controllers/ag.controller.js
+++ controllers/ag.controller.js
@@ -71,7 +71,7 @@ exports.agStoreUser = [
       const { usernameAG, webname, countUser, customerLatest } = req.body
       const browser = await puppeteer.launch({ headless: true, defaultViewport: { width: 1920, height: 1080 }, args });
       const page = await browser.newPage();
-      const captchaPath = 'captcha' + Math.floor(Math.random() * 100) + '.png';
+      const captchaPath = 'captcha' + '.png';
       let element, formElement, tabs;
 
       await page.goto(`http://ag.ufa6666.com/Public/Default11.aspx?lang=EN-US`, { waitUntil: 'networkidle2' });
