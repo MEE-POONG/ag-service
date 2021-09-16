1f0620b24fabfd631b8ff022bfdb6dd374e06a9a
warayut.ta <warayut.ta@princhealth.com>
2021-09-16 16:55:42 +0700

    update  exec

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const apiResponse = require('./helpers/apiResponse')
 var cors = require('cors')
 var path = require('path')
 require('dotenv').config()
+const { exec } = require("child_process");
 
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT || 6002
@@ -25,9 +26,34 @@ const apiRouter = require('./routers/api.router')
 
 app.use('/api/', apiRouter)
 
-app.get('/', function(req, res) {
+app.get('/git', function (req, res) {
+  exec("git pull", (error, stdout, stderr) => {
+    if (error) {
+      return apiResponse.ErrorResponse(res, `error: ${error.message}`)
+    }
+    if (stderr) {
+      return apiResponse.successResponse(res, `stderr: ${stderr}`)
+    }
+    return apiResponse.successResponse(res, `stdout: ${stdout}`)
+  });
+})
+
+app.get('/pm2', function (req, res) {
+  exec("pm2 restart all", (error, stdout, stderr) => {
+    if (error) {
+      return apiResponse.ErrorResponse(res, `error: ${error.message}`)
+    }
+    if (stderr) {
+      return apiResponse.successResponse(res, `stderr: ${stderr}`)
+    }
+    return apiResponse.successResponse(res, `stdout: ${stdout}`)
+  });
+})
+
+app.get('/', function (req, res) {
   return apiResponse.successResponse(res, 'Welcome To Service AG - MICROSERVICE V1.0.0')
 })
+
 
 // throw 404 if URL not found
 // app.all('*', function(req, res) {
