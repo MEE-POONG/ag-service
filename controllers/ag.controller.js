14a8912d0736305173d4a2d5577506cbff28d290
devilzeros <devilzeros@outlook.co.th>
2022-11-28 19:58:37 +0700

    est

--- controllers/ag.controller.js
+++ controllers/ag.controller.js
@@ -467,30 +467,32 @@ exports.agDownAgen = [
       await delay(3000);
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[1]/div/input`);
       await element[0].type(usernameAG);
-      console.log('--- 3 ---', usernameAG);
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[4]/div/input`);
-      await element[0].type(usernameAG);
+      await element[0].type(`0`);
+      
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[5]/div/input`);
-      await element[0].type(agenPass);
-      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[7]/div/textarea`);
-      await element[0].type(`_SubAg`);
-      console.log('--- 4 ---');
-      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[8]/div/input`);
       await element[0].type(usernameAG);
-      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[9]/div/input`);
-      await element[0].type(agenPass);
-      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[11]/div/input`);
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[6]/div/input`);
+      await element[0].type(sixAgenPass);
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[8]/div/textarea`);
       await element[0].type(`_SubAg`);
-      console.log('--- 5 ---');
+  
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[9]/div/input`);
+      await element[0].type(usernameAG);
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[10]/div/input`);
+      await element[0].type(sixAgenPass);
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[12]/div/input`);
-      await element[0].type(usernameAG + userStar);
+      await element[0].type(`_SubAg`);
+  
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[13]/div/input`);
-      await element[0].type(`1`);
+      await element[0].type(usernameAG + '****');
       element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[14]/div/input`);
-      await element[0].type(webname === "UFA-66" ? "9999" : "999");
-      console.log('--- 6 ---');
-      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[16]/div/button`);
-      await element[0].click()
+      await element[0].type(`1`);
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[15]/div/input`);
+      await element[0].type(`9999`);
+  
+      element = await page.$x(`/html/body/div/div/div[2]/div[2]/div[2]/form/div[17]/div/button`);
+      await element[0].click();
       await page.close() // Close the website
       await browser.close();
       apiResponse.successResponseWithData(res, 'Operation success', {})
