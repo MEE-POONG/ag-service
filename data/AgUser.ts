241038beead81159dc1b94ed6fc88ef81770063c
devilzeros <devilzeros@outlook.co.th>
2025-09-05 02:37:55 +0700

    AgUser data

---
+++ data/AgUser.ts
@@ -0,0 +1,5 @@
+export const AgUser = [
+    { AgUser: 'ufh27oa1', useLogin: 'ufh27oa1ufa66', reserve: 'ufh27oa1est', Origin: 'ufh27o', Position: 'agent', authKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+    { AgUser: 'ufh27o', useLogin: 'ufh27oufa66', reserve: 'ufh27oest', Origin: 'ufh27', Position: 'master', authKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+    { AgUser: 'ufh27', useLogin: 'ufh27ufa66', reserve: 'ufh27ufa66', Origin: '', Position: 'senior', authKey: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+];
