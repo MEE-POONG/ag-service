9105d8f176aaafabb11ff722d704dbbdf28cad50
devilzeros <devilzeros@outlook.co.th>
2025-10-15 11:41:23 +0700

    est

--- components/Page404.tsx
+++ components/Page404.tsx
@@ -39,7 +39,7 @@ export default function Page404({
                         
                         <button 
                             onClick={() => logout()}
-                            className="block w-full text-gray-500 hover:text-gray-700 transition-colors"
+                            className="block w-full text-gray-500 hover:text-gray-700 transition-colors border-2 border-red-500 rounded-lg px-4 py-2"
                         >
                             Logout
                         </button>
